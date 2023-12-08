4c7e4caf362bd61642044bd08e12e2ea1e321a3d
james.lee <james.lee@forbiz.co.kr>
2023-12-08 13:43:34 +0900

    환경설정

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from dotenv import load_dotenv
-def main():
-    print(add(1,2))
+from IPython.display import display, Image, Audio
 
-def add(a, b):
-    return a+b
+import cv2  # We're using OpenCV to read video
+import base64
+import time
+import io
+import openai
+import os
+import requests
+
+import streamlit as st
+import tempfile
+import numpy as np
+
+
+load_dotenv()
+
+
+
+def main():
+    print(os.environ['OPENAI_API_KEY'])
+
 
 if __name__ == "__main__":
     main()
